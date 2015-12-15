6d259b596274bc18411557e3957f82683ce8802d
crevillo <crevillo@gmail.com>
2015-12-15 18:43:59 +0100

    added sql script with the legacy data

---
+++ data/legacy-cleandata.sql
@@ -0,0 +1,2 @@
+INSERT INTO `ezpolicy` (`function_name`, `id`, `module_name`, `original_id`, `role_id`) VALUES ('pdf',329,'content',0,1);
+INSERT INTO `ezpolicy` (`function_name`, `id`, `module_name`, `original_id`, `role_id`) VALUES ('*',330,'ezoe',0,3);
