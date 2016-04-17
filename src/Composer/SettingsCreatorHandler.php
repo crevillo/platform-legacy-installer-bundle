e57a953435a83b3143081993bf3ad3ef56d6fbbe
crevillo <crevillo@gmail.com>
2016-04-17 16:15:18 +0200

    generate siteaccess site file and fixed some wrong settings

---
+++ src/Composer/SettingsCreatorHandler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carlosrevillo
+ * Date: 17/04/16
+ * Time: 12:23
+ */
+
+namespace Crevillo\PlatformLegacyInstallerBundle\Composer;
+
+
+class SettingsCreatorHandler
+{
+
+}
